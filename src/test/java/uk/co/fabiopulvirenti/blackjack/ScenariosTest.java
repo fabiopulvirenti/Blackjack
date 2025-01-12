fad4cf05068c9baf5718c0ae5e0cb67530ca5a2b
fabiopulvirenti <fpulvirenti002@gmail.com>
2025-01-12 20:45:04 +0000

    refactor ScenariosTest and add a new Scenario

--- src/test/java/uk/co/fabiopulvirenti/blackjack/ScenariosTest.java
+++ src/test/java/uk/co/fabiopulvirenti/blackjack/ScenariosTest.java
@@ -1,15 +1,31 @@
 package uk.co.fabiopulvirenti.blackjack;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import uk.co.fabiopulvirenti.blackjack.actors.Dealer;
 import uk.co.fabiopulvirenti.blackjack.actors.Player;
-import uk.co.fabiopulvirenti.blackjack.model.Card;
-import uk.co.fabiopulvirenti.blackjack.model.Rank;
-import uk.co.fabiopulvirenti.blackjack.model.Suit;
+import uk.co.fabiopulvirenti.blackjack.model.Deck;
+import uk.co.fabiopulvirenti.blackjack.model.PlayerChoice;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ScenariosTest {
+
+    private Player player;
+    private Dealer dealer;
+
+
+    @BeforeEach
+    void setUpEachTest() {
+        Deck deck = new Deck();
+        this.dealer = new Dealer();
+        this.dealer.setDeck(deck);
+        this.player = new Player("Fabio");
+    }
+
 
     @Test
     @DisplayName("""
@@ -18,15 +34,11 @@ public class ScenariosTest {
                 Then I have two cards
             """)
     void openingHandTest() {
-        Player player1= new Player("Fabio");
-        Card card1 = new Card(Rank.QUEEN, Suit.HEART);
-        Card card2 = new Card(Rank.ACE, Suit.DIAMOND);
+        assertEquals(0, this.player.getNumberOfCards());
 
-        assertEquals(0, player1.getNumberOfCards());
+        this.dealer.dealOpenHand(List.of(this.player));
 
-
-        player1.openingHand(card1,card2);
-        assertEquals(2,player1.getNumberOfCards());
+        assertEquals(2, this.player.getNumberOfCards());
     }
 
 
@@ -39,18 +51,50 @@ public class ScenariosTest {
             And my score is updated
             """)
     void hitCardTest(){
-        Player player1 = new Player("Fabio");
-        Card card1 = new Card(Rank.NINE, Suit.SPADE);
-        Card card2 = new Card(Rank.THREE, Suit.SPADE);
-        Card card3 = new Card(Rank.TWO, Suit.SPADE);
+        this.dealer.dealOpenHand(List.of(this.player));
+        assertEquals(2, this.player.getNumberOfCards());
+        int score = this.player.getScore();
 
-        assertEquals(0, player1.getScore());
+        this.dealer.valueChoiceOfPlayer(this.player, PlayerChoice.HIT);
 
-        player1.openingHand(card1,card2);
-        assertEquals(12, player1.getScore());
+        //Then I receive another card
+        assertEquals(3, this.player.getNumberOfCards());
 
-        player1.hitCard(card3);
-        assertEquals(14,player1.getScore());
+        //And my score is updated
+        assertTrue(score < this.player.getScore());
+
+//        Card card1 = new Card(Rank.NINE, Suit.SPADE);
+//        Card card2 = new Card(Rank.THREE, Suit.SPADE);
+//        Card card3 = new Card(Rank.TWO, Suit.SPADE);
+//
+//        assertEquals(0, player1.getScore());
+//
+//        player1.openingHand(card1,card2);
+//        assertEquals(12, player1.getScore());
+//
+//        player1.hitCard(card3);
+//        assertEquals(14,player1.getScore());
+    }
+
+    @Test
+    @DisplayName("""
+            Given I have a valid hand of cards
+            When I choose to ‘stand’
+            Then I receive no further cards
+            And my score is evaluated
+            """)
+    void standCardTest(){
+        this.dealer.dealOpenHand(List.of(this.player));
+        assertEquals(2, this.player.getNumberOfCards());
+        assertTrue(player.hasValidHand());
+
+        assertFalse(this.dealer.isPlayerEvaluated(this.player));
+
+        this.dealer.valueChoiceOfPlayer(this.player, PlayerChoice.STAND);
+        assertEquals(2, this.player.getNumberOfCards());
+
+        assertTrue(this.dealer.isPlayerEvaluated(this.player));
+
     }
 
 

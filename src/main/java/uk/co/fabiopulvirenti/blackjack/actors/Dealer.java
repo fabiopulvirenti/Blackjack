a1a247df4cc3d7e646e93cf44d23304a65b11524
fabiopulvirenti <fpulvirenti002@gmail.com>
2025-01-14 18:31:30 +0000

    add comments

--- src/main/java/uk/co/fabiopulvirenti/blackjack/actors/Dealer.java
+++ src/main/java/uk/co/fabiopulvirenti/blackjack/actors/Dealer.java
@@ -9,29 +9,30 @@ import java.util.List;
 import java.util.Map;
 
 /**
- *
+ * This class deals the cards to players created elsewhere and evaluate the choice 'hit' and 'stand' of each player
  */
 public class Dealer {
 
     private Deck deck;
-    private Map<Player,Integer> playersScore= new HashMap<>();
+    private Map<Player, Integer> playersScore = new HashMap<>();
 
     public void setDeck(Deck deck) {
         this.deck = deck;
     }
 
     /**
+     * It deals the opening Hand to the list of players given as input
      *
-     * @param playerList
+     * @param playerList List of Players for which the hand is dealt
      */
-    public void dealOpenHand(List<Player> playerList){
+    public void dealOpenHand(List<Player> playerList) {
 
-        if(playerList==null) {
+        if (playerList == null) {
             throw new IllegalArgumentException("Player List cannot be null.");
         }
         this.playersScore.clear();// it empties the map when a new hand starts
 
-        for(Player player : playerList){
+        for (Player player : playerList) {
             Card card1 = deck.getCard();
             Card card2 = deck.getCard();
             player.openingHand(card1, card2);
@@ -41,43 +42,46 @@ public class Dealer {
 
 
     /**
+     * It checks the choice of the Player and either gives a card to the player if it has chosen 'Hit' or evaluates its
+     * score it has chosen 'Stand'
      *
-     * @param player
-     * @param playerChoice
+     * @param player Player that makes the choice
+     * @param playerChoice the choice to evaluate
      */
-    public void valueChoiceOfPlayer(Player player, PlayerChoice playerChoice){
+    public void valueChoiceOfPlayer(Player player, PlayerChoice playerChoice) {
 
-        if(playerChoice==PlayerChoice.HIT){
+        if (playerChoice == PlayerChoice.HIT) {
             Card card = deck.getCard();
             player.hitCard(card);
-        } else if (playerChoice==PlayerChoice.STAND){
+        } else if (playerChoice == PlayerChoice.STAND) {
             evaluatePlayer(player);
         }
 
     }
 
-    public int evaluatePlayer(Player player){
+    /**
+     * It evaluates the choice of score of the player and add it to the players' scores map.
+     *
+     * @param player Player for which evaluation happens
+     * @return the score of the current player
+     */
+    public int evaluatePlayer(Player player) {
         int score = player.getScore();
-        playersScore.put(player,score);
+        playersScore.put(player, score);
 
         return score;
     }
 
 
-
     /**
+     * It checks if the player has been already evaluated
      *
-     * @param player
-     * @return
+     * @param player Player for which checking
+     * @return true if the player has been evaluated, false otherwise
      */
-    public boolean isPlayerEvaluated(Player player){
+    public boolean isPlayerEvaluated(Player player) {
         return playersScore.containsKey(player);
     }
-
-
-
-
-
 
 
 }
